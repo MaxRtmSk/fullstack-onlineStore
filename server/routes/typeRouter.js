1a5a3cbcc8a02bff51c58e7b6c8c6d3e0e00fd12
ratomsky <max.work.dev@gmail.com>
2021-05-04 19:13:17 +0300

    feat: add middleware check role

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const typeController = require('../controllers/typeController')
+const chekRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', typeController.create)
+router.post('/', chekRole('ADMIN'), typeController.create)
 router.get('/', typeController.getAll)
 router.delete('/', typeController.delete)
 
