b5ae4324bdc6d7b3e6b645cf235bbd5a3aba0cd3
ratomsky <max.work.dev@gmail.com>
2021-05-07 07:45:12 +0300

    feat: change structure

--- db.js
+++ db.js
@@ -1,12 +1,33 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
+
+// sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   protocol: 'postgres',
+//   dialectOptions: {
+//       ssl: true
+//   }
+// });
+
+console.log("2" + process.env.DATABASE_URL);
 
 module.exports = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: 'postgres',
+    dialect: "postgres",
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
   }
 )
+
+// new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     dialect: "postgres",
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//   }
+// )
