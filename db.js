e9522c0364b5e1603851bfd6c988985d8a8861c3
ratomsky <max.work.dev@gmail.com>
2021-05-07 08:04:38 +0300

    feat: upadate

--- db.js
+++ db.js
@@ -10,16 +10,13 @@ const { Sequelize } = require("sequelize");
 
 console.log("2" + process.env.DATABASE_URL);
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+      ssl: true
   }
-)
+});
 
 // new Sequelize(
 //   process.env.DB_NAME,
